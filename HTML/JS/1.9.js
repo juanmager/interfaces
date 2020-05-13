db27ccc719a6730c5c4226fd9db54f5a9d445290
juanmager <juanmadaria@hotmail.es>
2020-05-13 15:00:45 -0300

    Exercise 1.9

---
+++ HTML/JS/1.9.js
@@ -0,0 +1,13 @@
+
+alertSitio();
+nameU();
+alertSaludo();  
+
+function alertSitio() {
+    alert("Bienvenidos a un sitio super mega archi divertido!!!. Pero disfrutalo con cautela.")
+}
+
+function nameU() {
+   var nameUser = prompt("Ingresa tu nombre por favor:")
+   alert("Hola " + nameUser + "!")
+}
