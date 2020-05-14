7a9abff73ac40cb7bf6517eb4af735c3783c459c
juanmager <juanmadaria@hotmail.es>
2020-05-13 22:29:39 -0300

    Update

--- HTML/JS/1.9.js
+++ HTML/JS/1.9.js
@@ -1,13 +1,15 @@
 
 alertSitio();
 nameU();
-alertSaludo();  
 
 function alertSitio() {
     alert("Bienvenidos a un sitio super mega archi divertido!!!. Pero disfrutalo con cautela.")
 }
 
 function nameU() {
-   var nameUser = prompt("Ingresa tu nombre por favor:")
-   alert("Hola " + nameUser + "!")
+    var nameUser = prompt("Ingresa tu nombre por favor:")
+    alert(`Hola ${nameUser}!`)
+
+    var ageUser = prompt("Ingresa tu edad por favor:")
+    alert(`Hola, soy ${nameUser} y tengo ${ageUser} años!`)
 }
