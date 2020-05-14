ce6897d4a9b388716b4ddd1b77e69a685a8b7f7d
juanmager <juanmadaria@hotmail.es>
2020-05-13 23:59:21 -0300

    Update

--- HTML/JS/1.9.js
+++ HTML/JS/1.9.js
@@ -2,6 +2,7 @@
 alertSitio();
 nameU();
 
+//Ejercicio datos user.-
 function alertSitio() {
     alert("Bienvenidos a un sitio super mega archi divertido!!!. Pero disfrutalo con cautela.")
 }
@@ -9,7 +10,16 @@ function alertSitio() {
 function nameU() {
     var nameUser = prompt("Ingresa tu nombre por favor:")
     alert(`Hola ${nameUser}!`)
-
+    
     var ageUser = prompt("Ingresa tu edad por favor:")
     alert(`Hola, soy ${nameUser} y tengo ${ageUser} años!`)
 }
+
+// Ejercicio Calculador de Edad
+calculadorEdad();
+function calculadorEdad(anios) {
+    var anios = prompt("Cuantos años tenes?: ")
+    var cantDias = anios * 365;
+    alert(`Viviste en total ${cantDias} dias`)
+} 
+
